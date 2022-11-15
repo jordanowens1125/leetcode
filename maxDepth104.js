deb989e1e81226f24bd163edde01946e96b3b898
jordanowens1125 <60613533+jordanowens1125@users.noreply.github.com>
2022-11-15 09:54:30 -0600

    Create maxDepth104.js
    Solved using recursion and binary tree props

---
+++ maxDepth104.js
@@ -0,0 +1,14 @@
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+var maxDepth = function(root) {
+    if(!root){
+        return 0
+    }
+    else{
+        return 1+ Math.max(maxDepth(root.left), maxDepth(root.right))
+    }
+};
