7f689e7d8c74bd6e1b79268aecf0cd69e0a23955
jordanowens1125 <60613533+jordanowens1125@users.noreply.github.com>
2023-05-28 20:57:40 -0500

    Create 876-MiddleofLinkedList.js

---
+++ 876-MiddleofLinkedList.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/middle-of-the-linked-list/description/
+var middleNode = function(head) {
+    let slow = head
+    let fast = head
+    while(fast&&fast.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return slow
+};
